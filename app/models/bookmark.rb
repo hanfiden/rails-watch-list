5c79bbab13d523bf7934ebe2b92f29ad487d6ce1
fidenhan <fidendu2@gmail.com>
2021-08-12 11:58:12 +0200

    add model list, movie, bookmark with association and validation

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :comment, length: { minimum: 6 }
+end
