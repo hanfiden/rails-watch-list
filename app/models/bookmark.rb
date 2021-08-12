cd14652eaa6d1afa6247a407d7d1202b9837e552
fidenhan <fidendu2@gmail.com>
2021-08-12 12:42:28 +0200

    add routes and generate controller lists, movies, bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,6 +4,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :list_id, uniqueness: { scope: :movie_id }
   validates :comment, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists do
+    collection do
+      get :movies
+    end
+    resources :bookmarks, only: [:create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
