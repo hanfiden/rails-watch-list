bae853117b3fbfb0db5998ad86e17712163cdcb2
fidenhan <fidendu2@gmail.com>
2021-08-13 11:13:44 +0200

    update style and change rating datatype

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_093544) do
+ActiveRecord::Schema.define(version: 2021_08_13_090546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_08_12_093544) do
     t.string "title"
     t.text "overview"
     t.text "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
